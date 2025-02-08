1aaeea8ed681573aae5e2d3763019ed2498fd99c
Ivan K <kosi17100@gmail.com>
2025-02-08 17:18:00 +0300

    Part_2 complete

--- SQL_BD_Monsters/Part_2.sql
+++ SQL_BD_Monsters/Part_2.sql
@@ -1,5 +1,7 @@
 
 UPDATE ___Monsters
+INNER JOIN ___Tournaments
+ON ___Tournaments.winner = ___Monsters.id
 SET count_wins = count_wins + 1
 WHERE ___Monsters.id = ___Tournaments.winner;
 
@@ -10,9 +12,27 @@ ADD COLUMN `Arena` INTEGER UNSIGNED;
 
 CREATE TABLE `___Arena` (
 	`id` INTEGER UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	`name` ENUM(`Kolizey`, `Sandbox`) NOT NULL
+	`name` ENUM('Kolizey', 'Sandbox') NOT NULL);
+	
+INSERT INTO `___Arena` (`id`, `name`) VALUES ('1', 'Kolizey');	
+INSERT INTO `___Arena` (`id`, `name`) VALUES ('2', 'Sandbox');	
 
-	CONSTRAINT `fk_arena_tournament` 
-	FOREIGN KEY (`id`) 
-	REFERENCES `___Tournaments` (`Arena`)
-) 
+
+UPDATE `___Tournaments` t
+SET t.Arena = 'Kolizey'
+WHERE t.prize < (SELECT * FROM (SELECT AVG(prize) FROM `___Tournaments`) AS t1);
+
+UPDATE `___Tournaments` t
+SET t.Arena = 'Sandbox'
+WHERE t.prize >= (SELECT * FROM (SELECT AVG(prize) FROM `___Tournaments`) AS t1);
+
+ALTER TABLE ___Tournaments
+ADD COLUMN `id_arena` INTEGER UNSIGNED;
+
+ALTER TABLE `___Tournaments`
+ADD CONSTRAINT fk_arena_idtournament
+FOREIGN KEY(`id_arena`)
+REFERENCES  `___Arena`(`id`);
+
+CREATE INDEX name_index
+ON ___Monsters(`name`);
